b365ebd384237e29537fe3d17cc8f72bef9915b8
Itay Mevorach <itaymevorach@dyn-10-108-38-248.wless.uoregon.edu>
2024-11-04 10:11:22 -0800

    i think its fair to update ver

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Bamboo',
-    version='0.1.0',
+    version='0.1.1',
     author='Itay Mevorach',
     author_email='itaym@uoregon.edu',
     description='Advanced data cleaning built on top of Pandas',
